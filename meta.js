cd97505961a58ff452cbf1fe7903f8fdd6541d9b
donghanji <donghanji@donghanjideMacBook-Pro.local>
2021-07-07 01:29:40 +0800

    微信模板内容测试

---
+++ meta.js
@@ -0,0 +1,16 @@
+// 项目创建完成后输出信息
+exports.run ={
+  "yarn test":"Start to test the project"
+};
+// 项目创建过程中的prompts
+// 参考:https://github.com/SBoudrias/Inquirer.js/blob/master/README.md
+exports.prompts = {
+  "name": {
+      "message": "What's the project name?",
+      "default": "`${this.name}`"
+  },
+  "description": {
+      "name": "description",
+      "message": "What's your project description?"
+  }
+}
