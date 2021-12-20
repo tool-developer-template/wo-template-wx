6a1ea4b0fd7804d32ea652706cdf2a46b0800b68
tooldeveloper <tooldeveloper@163.com>
2021-12-20 16:49:40 +0800

    移除无效信息

--- template/src/pages/index/index.js
+++ template/src/pages/index/index.js
@@ -3,14 +3,7 @@
 const app = getApp()
 //
 const page = app.extend(app.App,{
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
+  data: {},
   events:{
     'tap #openid':"getOpenId",
     'tap #clear':'clearOpenId'
