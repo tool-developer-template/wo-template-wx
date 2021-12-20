fdc093bd1c57a8435ad1f237c835e4d70b1f4f02
tooldeveloper <tooldeveloper@163.com>
2021-12-20 16:57:50 +0800

    无效代码移除

--- template/src/app.js
+++ template/src/app.js
@@ -16,13 +16,15 @@ const options = wxapp.extend(
   wxapp,{
     App:wxapp,// 提供给子页面，直接通过app.App获取
     globalData: {
-      userInfo: null
+      //
     },
     onLaunch() {
       // 使用云开发
       if (!wx.cloud) {
+        //
         console.error('请使用 2.2.3 或以上的基础库以使用云能力');
       } else {
+        //
         wx.cloud.init({
           // env 参数说明：
           //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
