a5f009829e1ea2c70ae6c865c6a3f81f3da5fa23
tooldeveloper <tooldeveloper@163.com>
2021-12-20 16:25:15 +0800

    接入云开发分支

--- template/src/defapp.js
+++ template/src/defapp.js
@@ -8,6 +8,28 @@ export default {
     //
     return next(e);
   },
+  // 云函数调用
+  cc({name,data = {},config = {}}){
+    //
+    this.showLoading();
+    //
+    return wx.cloud.callFunction({
+      name,
+      config: {
+        ...(config || {}),
+        env: this.envId,// 这里直接没有注入到data上
+      },
+      data
+    }).then((resp)=>{
+      //
+      this.hideLoading();
+      //
+      return resp.result;
+    }).catch(()=>{
+      //
+      this.hideLoading();
+    })
+  },
   // 页面使用onPageShow
   onShow(options) {
     //
@@ -17,6 +39,7 @@ export default {
   // 页面使用onPageLoad
   onLoad(options){
     //
+    this.envId = options.envId;
     //
     this.onPageLoad && this.onPageLoad(options);
   },
